9eece711685e84f62e46918b7155ee3ef67ea097
Atik Shakur <atik.shakur98@gmail.com>
2019-05-09 23:59:19 +0600

    Firebase added & Splash fixed

--- app/src/main/java/com/example/driverjaben/entrypoint/SplashActivity.java
+++ app/src/main/java/com/example/driverjaben/entrypoint/SplashActivity.java
@@ -1,9 +1,14 @@
 package com.example.driverjaben.entrypoint;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.driverjaben.EightActivity;
 import com.example.driverjaben.R;
+import com.example.driverjaben.login.EnterPhoneForLogin;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +16,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, EnterPhoneForLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }

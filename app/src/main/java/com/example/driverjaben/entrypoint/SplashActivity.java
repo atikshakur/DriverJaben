0eb3831caf1e4b7e585ce05aee91f6ff693abc87
Atik Shakur <atik.shakur98@gmail.com>
2019-05-09 22:31:23 +0600

    Splash screen added

--- app/src/main/java/com/example/driverjaben/entrypoint/SplashActivity.java
+++ app/src/main/java/com/example/driverjaben/entrypoint/SplashActivity.java
@@ -1,13 +1,15 @@
-package com.example.driverjaben;
+package com.example.driverjaben.entrypoint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.driverjaben.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }

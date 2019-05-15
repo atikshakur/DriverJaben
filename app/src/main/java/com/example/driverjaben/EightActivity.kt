d8c44b84f1e23cceb28348d07789069c3ca08109
Atik Shakur <atik.shakur98@gmail.com>
2019-05-15 22:47:59 +0600

    Converted some files from java to kotlin

---
+++ app/src/main/java/com/example/driverjaben/EightActivity.kt
@@ -0,0 +1,12 @@
+package com.example.driverjaben
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EightActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eight)
+    }
+}
---
+++ app/src/main/java/com/example/driverjaben/TenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.driverjaben
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ten)
+    }
+}
